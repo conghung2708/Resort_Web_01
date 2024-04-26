b7e022fd6f7eda555b18ba1b86abd0bb60021e6e
conghung2708 <conghung05005@gmail.com>
2024-04-26 11:55:46 +0700

    Add project files.

---
+++ ResortBooking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResortBooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
